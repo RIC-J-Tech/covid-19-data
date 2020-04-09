e9c16e6bc3f105fff1ad64decc3459390c3193de
Cathy Tasama <ctasama@cnm.edu>
2020-04-08 23:34:37 -0600

    Wrote persona-1

---
+++ documentation/adriel-persona-1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Persona</title>
+</head>
+<body>
+<h1>Persona: Adriel Stephens; COVID 19 app user</h1>
+<h2>Demographics</h2>
+<ul>
+	<li>Age: 40yrs old</li>
+	<li>location: Rio Rancho, New Mexico</li>
+	<li>ofession: Fullstack Developer</li>
+</ul>
+
+<h2>Goals</h2>
+<p>To be informed about the current infection rate of COVID 19 around him.</p>
+<h2>Technology</h2>
+<ul>
+	<li>Phone: Iphone 11</li>
+	<li>laptop: Dell intel core i5</li>
+	<li>OS: Windows 10</li>
+	<li>Internet: Proficient</li>
+</ul>
+
+<h2>Frustrations</h2>
+<p>Unable to get accurate and specific info from dif sites about his county.</p>
+
+<h1>User Story</h1>
+<p>As a resident of the Sandoval county in New Mexico and a concerned citizen,
+	I want to know the current infection rate of the corona virus around me.</p>
+
+<h1>Use Case Interaction Flow</h1>
+<p></p>
+
+</body>
+</html>
