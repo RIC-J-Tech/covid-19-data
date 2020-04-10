ee82966438a732a0068a97557c77254ead1d802d
opeyemi-jonah <gavrieljonah@gmail.com>
2020-04-10 11:09:35 -0600

    typo corrections

---
+++ documentation/tola.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+	<title>Tola's Persona</title>
+</head>
+
+<body>
+<h1>Purpose, Audience & Goals</h1>
+<p><strong>Purpose:</strong> This project will focus on showing areas on the map covid-19 status</p>
+<br>
+<p><strong>Audience:</strong> This site is for the state of New Mexico targeting the residents who are                        seeking health information
+ of their area or neighborhood.</p><br>
+<p><strong>Goal:</strong> The primary goal is to give a visual display of reports of the Covid-19 virus in the area</p>
+<br>
+<img src="../images/tola.jpg" alt="Photo of Tola" height="500"          width="650">
+<h2>Persona</h2>
+<p><strong>Name:</strong>Tola</p>
+<p><strong>Age:</strong>21</p>
+<p><strong>Profession:</strong> Reporter at KOB4</p>
+<p><strong>Technology:</strong> Dell laptop and an Iphone 8 with 4G network</p>
+
+<h3>Attitudes and Behavior: </h3>
+<p>Tola likes to make sure all facts are correct in her research.<br>
+ She's tech savy but still prefers simplicity over complexity</p>
+
+<h3>Frustration and Needs: </h3>
+<p>Tola needs a clear source of information about the covid-19 virus in New Mexico and also a sign of relief that <br>
+ situation is being handled properly. She wants to see sites of see sites of quarantine other than people's homes.</p>
+<p><strong>Goals: </strong>Tola's goal is to report the virus as well as give the people of new Mexico hope by sharing<br>
+ prevention and showing what the state has in place to take care of the affected people and the state's prvention measures.</p>
+<p><strong>User Story: </strong>As a user, I want to see a map showing areas of tested positive, non-tested, and the <br>
+	general population as well as quarantine spots or hospitals dealing with Covid-19 cases.</p>
+
+</body>
+</html>
