03d7fd9ea73ede9d0b1787506d8b75dd35f2a6fd
Cathy Tasama <ctasama@cnm.edu>
2020-04-21 10:31:10 -0600

    corrected tables

--- sql/capstone.sql
+++ sql/capstone.sql
@@ -1,13 +1,20 @@
 USE ctasama;
 
+DROP TABLE IF EXISTS vote;
+DROP TABLE IF EXISTS behavior;
+DROP TABLE IF EXISTS business;
+DROP TABLE IF EXISTS profile;
+
+
 CREATE TABLE profile(
 	profileId BINARY(16) NOT NULL,
 	profileActivationToken CHAR(32),
 	profileAvatarUrl VARCHAR(256) NOT NULL,
 	profileEmail VARCHAR(128) NOT NULL,
-	profileHash CHAR(64) NOT NULL,
+	profileHash CHAR(97) NOT NULL,
 	profilePhone VARCHAR(32),
 	profileUsername VARCHAR(32) NOT NULL,
+	UNIQUE(profilePhone),
 	UNIQUE(profileEmail),
 	UNIQUE(profileUsername),
 	INDEX(profileEmail),
@@ -17,34 +24,31 @@ CREATE TABLE profile(
 
 CREATE TABLE business(
    businessId BINARY(16) NOT NULL,
-   businessAddress VARCHAR(134) NOT NULL,
+   businessAddress JSON NOT NULL,
    businessLng DECIMAL(9,6),
    businessLat DECIMAL(9,6),
-   businessName VARCHAR(64) NOT NULL,
+   businessName VARCHAR(128) NOT NULL,
    businessUrl VARCHAR(256) NOT NULL,
-   UNIQUE(businessLng),
-   UNIQUE(businessLat),
-   INDEX(businessAddress),
+   INDEX(businessName),
    PRIMARY KEY(businessId)
 );
 
 
 CREATE TABLE behavior(
    behaviorId BINARY(16) NOT NULL,
-   behaviorBuisnessId BINARY(16) NOT NULL,
+   behaviorBusinessId BINARY(16) NOT NULL,
    behaviorProfileId BINARY(16) NOT NULL,
-	behaviors VARCHAR(256) NOT NULL,
+	behaviors TEXT NOT NULL,
 	behaviorDate DATETIME(6) NOT NULL,
-	INDEX(behaviorBuisnessId),
+	INDEX(behaviorBusinessId),
 	INDEX(behaviorProfileId),
-	FOREIGN KEY(behaviorBuisnessId) REFERENCES business(businessId),
+	FOREIGN KEY(behaviorBusinessId) REFERENCES business(businessId),
 	FOREIGN KEY(behaviorProfileId) REFERENCES profile(profileId),
 	PRIMARY KEY(behaviorId)
 );
 
 
 CREATE TABLE vote(
-	voteId BINARY(16)NOT NULL,
 	voteProfileId BINARY(16) NOT NULL,
 	voteBehaviorId BINARY(16) NOT NULL,
 	voteDate DATETIME(6) NOT NULL,
