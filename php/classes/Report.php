152f54539b9572343b90f6d82b934fcf99447856
Opeyemi Jonah <gavrieljonah@gmail.com>
2020-04-25 07:52:22 -0600

    created mutator

--- php/classes/Report.php
+++ php/classes/Report.php
@@ -3,6 +3,7 @@ namespace OpeyemiJonah\ObjectOriented;
 require_once ("autoload.php");
 
 use DateTime;
+use Exception;
 use InvalidArgumentException;
 use Ramsey\uuid\uuid;
 use RangeException;
@@ -78,7 +79,7 @@ public function __construct($newReportId,$newReportBusinessId,$newReportProfileI
 			$this->setReportDate($newReportDate);
 
 	}
-	catch(InvalidArgumentException | \RangeException| \Exception | \TypeError $exception) {
+	catch(InvalidArgumentException | RangeException| Exception | \TypeError $exception) {
 		$exceptionType = get_class($exception);
 		throw(new $exceptionType($exception->getMessage(), 0, $exception));
 	}
@@ -121,17 +122,140 @@ public function getReportDate(): DateTime{
 
 
 	/**
-	 * @param $reportId
-	 */
-
-	public function setReportId(){
-
+	 * mutator method for report id
+	 *
+	 * @param Uuid|string $newReportId new value of report id
+	 * @throws RangeException if $newReportId is not positive
+	 * @throws \TypeError if $newReportId is not a uuid or string
+	 **/
+	public function setReportId($newReportId): void {
+		try {
+			$uuid = self::validateUuid($newReportId);
+		} catch(InvalidArgumentException | RangeException | Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+		// convert and store the report id
+		$this->reportId = $uuid;
 	}
 	/**
-	 * @param uuid $reportBusinessId
-	 */
-	public function setReportBusinessId(uuid $reportBusinessId): void {
-		$this->reportBusinessId = $reportBusinessId;
+	 * mutator method for report business id
+	 *
+	 * @param string | Uuid $newReportBusinessId new value of behavior business id
+	 * @throws RangeException if $newReportId is not positive
+	 * @throws \TypeError if $newReportBusinessId is not an integer
+	 **/
+	public function setReportBusinessId($newReportBusinessId): void {
+		try {
+			$uuid = self::validateUuid($newReportBusinessId);
+		} catch(InvalidArgumentException | RangeException | Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+		// convert and store the business id
+		$this->reportBusinessId = $uuid;
+	}
+	/**
+	 * mutator method for report profile id
+	 *
+	 * @param string | Uuid $newReportProfileId new value of behavior business id
+	 * @throws RangeException if $newProfileId is not positive
+	 * @throws \TypeError if $newReportProfileId is not an integer
+	 **/
+	public function setReportProfileId($newReportProfileId): void {
+		try {
+			$uuid = self::validateUuid($newReportProfileId);
+		} catch(InvalidArgumentException | RangeException | Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+		// convert and store the profile id
+		$this->reportProfileId = $uuid;
+	}
+	/**
+	 * mutator method for report content
+	 *
+	 * @param string $newReportContent new value of report content
+	 * @throws InvalidArgumentException if $newReportContent is not a string or insecure
+	 * @throws \TypeError if $newReportContent is not a string
+	 **/
+	public function setReportContent(string $newReportContent): void {
+		// verify the report content is secure
+		$newReportContent = trim($newReportContent);
+		$newReportContent = filter_var($newReportContent, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newReportContent) === true) {
+			throw(new InvalidArgumentException("report content is empty or insecure"));
+		}
+		// store the report content
+		$this->reportContent = $newReportContent;
+	}
+	/**
+	 * mutator method for report date
+	 *
+	 * @param DateTime|string|null $newReportDate report date as a DateTime object or string (or null to load the current time)
+	 * @throws InvalidArgumentException if $newReportDate is not a valid object or string
+	 * @throws RangeException|Exception if $newReportDate is a date that does not exist
+	 **/
+	public function setReportDate($newReportDate = null): void {
+		// base case: if the date is null, use the current date and time
+		if($newReportDate === null) {
+			$this->reportDate = new DateTime();
+			return;
+		}
+		// store the like date using the ValidateDate trait
+		try {
+			$newReportDate = self::validateDateTime($newReportDate);
+		} catch(InvalidArgumentException | RangeException $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+		$this->reportDate = $newReportDate;
+	}
+	/**
+	 * inserts this report into mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function insert(\PDO $pdo): void {
+		// create query template
+		$query = "INSERT INTO report(reportId, reportBusinessId, reportProfileId, reportContent, reportDate) VALUES(:reportId, :reportBusinessId, :reportProfileId, :reportContent, :reportDate)";
+		$statement = $pdo->prepare($query);
+		// bind the member variables to the place holders in the template
+		$formattedDate = $this->reportDate->format("Y-m-d H:i:s.u");
+		$parameters = ["reportId" => $this->reportId->getBytes(), "reportBusinessId" => $this->reportBusinessId->getBytes(), "reportProfileId" => $this->reportProfileId->getBytes(), "reportContent" => $this->reportContent, "reportDate" => $formattedDate];
+		$statement->execute($parameters);
+	}
+	/**
+	 * deletes this report from mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function delete(\PDO $pdo): void {
+		// create query template
+		$query = "DELETE FROM report WHERE reportId = :reportId";
+		$statement = $pdo->prepare($query);
+		// bind the member variables to the place holder in the template
+		$parameters = ["reportId" => $this->reportId->getBytes()];
+		$statement->execute($parameters);
+	}
+	/**
+	 * updates this Report in mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function update(\PDO $pdo): void {
+		// create query template
+		$query = "UPDATE report SET reportBusinessId = :reportBusinessId, reportProfileId = :reportProfileId, reportContent = :reportContent, reportDate = :reportDate WHERE reportId = :reportId";
+		$statement = $pdo->prepare($query);
+		$formattedDate = $this->reportDate->format("Y-m-d H:i:s.u");
+		$parameters = ["reportId" => $this->reportId->getBytes(), "reportBusinessId" => $this->reportBusinessId->getBytes(), "reportProfileId" => $this->reportProfileId->getBytes(), "reportContent" => $this->reportContent, "reportDate" => $formattedDate];
+		$statement->execute($parameters);
 	}
 
 
