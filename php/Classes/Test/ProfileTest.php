38b25e21da3aface321bfd01ec4718f0cf68df79
Opeyemi Jonah <gavrieljonah@gmail.com>
2020-04-30 01:40:55 -0600

    typo

--- php/Classes/Test/ProfileTest.php
+++ php/Classes/Test/ProfileTest.php
@@ -214,7 +214,7 @@ public function getProfileValidateByUsername(): void{
 			$this->VALID_ACTIVATION_TOKEN,$this->VALID_PROFILE_EMAIL,$this->VALID_PROFILE_HASH,$this->VALID_PROFILE_PHONE,
 			$this->VALID_PROFILE_USERNAME);
 		$profile->insert($this->getPDO());
-		$profile->getAuthorByAuthorId($this->getPDO(),$profileId);
+		$profile->getProfileByProfileId($this->getPDO(),$profileId);
 		//check count of profile record in the db after the insert
 		$numRowsAfter = $this->getConnection()->getRowCount("profile");
 		self::assertEquals($numRows + 1, $numRowsAfter,"checked record count");
