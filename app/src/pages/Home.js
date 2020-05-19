c793a5c70b4b41a1d46e10fa71c5added54f5f4e
Opeyemi Jonah <gavrieljonah@gmail.com>
2020-05-19 14:04:13 -0600

    added action and shared folder

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -6,8 +6,9 @@ import {BehaviorPost} from "../components/behaviorpost";
 export const Home = () => {
 	return (
 		<>
-<BehaviorPost behaviorContent="Wear mask" behaviorRating = {5} />
-				<Card style={{ width: '50rem' }}>
+	<BehaviorPost behaviorContent="Wear mask" behaviorRating = {5} />
+
+				<Card style={{ width: '50rem' ,zIndex:'4'}}>
 					<Card.Img variant="top" src="holder.js/100px180" alt="photo" />
 					<div className="red">
 					<Card.Body>
