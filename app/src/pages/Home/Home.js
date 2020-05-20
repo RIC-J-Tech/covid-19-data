5960a538ff1aa23467123e721cb4a5ae39a06dbe
opeyemi-jonah <gavrieljonah@gmail.com>
2020-05-20 14:38:20 -0600

    deleted some pages

--- app/src/pages/Home/Home.js
+++ app/src/pages/Home/Home.js
@@ -3,45 +3,39 @@ import {useSelector, useDispatch} from "react-redux";
 import {BusinessList} from "./businessList";
 import {getAllBusinesses} from "../../shared/actions/get-businesses";
 import Card from "react-bootstrap/Card";
-import {business} from "../../components/business";
 
-export const Home= () => {
+export const Home = () => {
 
-	// use selector to set users to users stored in state
-	const users = useSelector(state => state.users);
-
-	// use dispatch from redux to dispatch actions
+	const business = useSelector(state => state.business);
 	const dispatch = useDispatch();
 
-	// get users
 	const effects = () => {
-		dispatch(getAllBusinesses())
+		dispatch(getAllBusinesses());
 	};
 
-	// set inputs to an empty array before update
 	const inputs = [];
 
-	// do this effect on component update
-	useEffect(effects, inputs);
+	useEffect(effects,inputs);
 
 	return (
-		<main className="container">
-			<table className="table table-responsive table-hover table-dark">
-				<thead>
-				<tr>
-					<th><h4>User Id</h4></th>
-					<th><h4>Name</h4></th>
-					<th><h4>Email</h4></th>
-					<th><h4>Phone</h4></th>
-					<th><h4>Username</h4></th>
-					<th><h4>Website</h4></th>
-				</tr>
-				</thead>
-				<BusinessList businesses={business}/>
-			</table>
-		</main>
+		<>
+			{business.map(business => {
+				return(
+					<Card style={{ width: '18rem' }} key={business.businessId}>
+						<Card.Img variant="top" src={business.profileAvatarUrl} />
+						<Card.Body>
+							<Card.Text>{business.businessName}</Card.Text>
+							<Card.Text>{new Date(business.businessDate).toDateString()}</Card.Text>
+							<Card.Text>
+								{business.businessContent}
+							</Card.Text>
+						</Card.Body>
+					</Card>)
+			})}
+		</>
+
+
 	)
 };
-
 
 
--- app/src/pages/ProfileHomePage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Navbars from "../shared/components/Navbars";
-export const ProfileHomePage = ({match}) => {
-	console.log(match);
-
-	return <div>
-		<Navbars />
-	</div>
-};
