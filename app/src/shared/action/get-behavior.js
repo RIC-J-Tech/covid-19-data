58358712fb538bbccc38b3315cb9ed15c79909e2
Opeyemi Jonah <gavrieljonah@gmail.com>
2020-05-19 20:43:08 -0600

    re-did get behavior

--- app/src/shared/action/get-behavior.js
+++ app/src/shared/action/get-behavior.js
@@ -4,3 +4,23 @@ export const getAllBehaviors = () => async (dispatch) => {
 	const payload =  await httpConfig.get("/apis/behavior/");
 	dispatch({type: "GET_ALL_BEHAVIORS",payload : payload.data });
 };
+
+//Get payload from behavior Id
+export const getBehaviorByBehaviorId =()=> async (dispatch) => {
+	const payload = await httpConfig.get(`/apis/behavior/${Id}`)
+	dispatch({type: "GET_BEHAVIOR_BY_BEHAVIOR_ID", payload : payload.data});
+
+};
+
+
+export const getBehaviorByBehaviorProfileId =()=> async (dispatch) => {
+	const payload = await httpConfig.get(`/apis/behavior/${behaviorProfileId}`)
+	dispatch({type: "GET_BEHAVIOR_BY_BEHAVIOR_PROFILE_ID", payload : payload.data});
+
+};
+
+export const getBehaviorByBehaviorBusinessId =()=> async (dispatch)=>{
+	const payload = await httpConfig.get(`/apis/behavior/${behaviorBusinessId}`)
+	dispatch({type: "GET_BEHAVIOR_BY_BEHAVIOR_BUSINESS_ID", payload : payload.data})
+
+}
