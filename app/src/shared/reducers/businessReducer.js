a568f73fbcb2203ade6878fa52f1182070252571
opeyemi-jonah <gavrieljonah@gmail.com>
2020-05-23 09:51:12 -0600

    fixed merge conflict

--- app/src/shared/reducers/businessReducer.js
+++ app/src/shared/reducers/businessReducer.js
@@ -6,11 +6,9 @@ export default (state = [], action) => {
 		case "GET_BUSINESS_BY_BUSINESS_ID":
 			return [...action.payload];
 		case "GET_BUSINESS_BY_BUSINESS_NAME":
-<<<<<<< HEAD
-			return  action.payload;
-=======
+
 			return [...state, ...action.payload];
->>>>>>> fixed behavior list to show
+
 		default:
 			return state;
 	}
