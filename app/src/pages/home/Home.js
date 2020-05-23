0af35d3033719a66ad755cbea54331c58eddbd63
opeyemi-jonah <gavrieljonah@gmail.com>
2020-05-22 22:08:54 -0600

    added behavior component

--- app/src/pages/home/Home.js
+++ app/src/pages/home/Home.js
@@ -1,43 +1,36 @@
 import React, {useEffect} from 'react';
 import {useSelector, useDispatch} from "react-redux";
 import {ProfileList} from "../profile/ProfileList";
-import {getAllProfiles} from "../../shared/actions/get-profile";
-
+// import {getAllProfiles, getProfileByEmail} from "../../shared/actions/get-profile";
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+import Behavior, { Behaviors } from '../behavior/Behaviors'
+import Profile from '../profile/Profile'
 
 export const Home= () => {
 
-	// use selector to set users to users stored in state
-	const profiles = useSelector(state => state.profiles);
 
-	// use dispatch from redux to dispatch actions
-	const dispatch = useDispatch();
-
-	// get profiles
-	const effects = () => {
-		dispatch(getAllProfiles())
-	};
-
-	// set inputs to an empty array before update
-	const inputs = [];
-
-	// do this effect on component update
-	useEffect(effects, inputs);
 
 	return (
-			<main className="container">
-				<table className="table table-responsive table-hover table-dark">
-					<thead>
-					<tr>
-						<th><h4>Profile Id</h4></th>
-						<th><h4>User Name</h4></th>
-						<th><h4>Email</h4></th>
-						<th><h4>Phone No.</h4></th>
-						<th><h4>Avatar Url</h4></th>
-					</tr>
-					</thead>
-					<ProfileList profiles={profiles}/>
-				</table>
-				<h1>Home</h1>
+		<main className="container">
+
+			<h1>Home</h1>
+
+			<Grid container spacing={10}>
+				<Grid item sm={8} xs={12}>
+
+					<p>Content...</p>
+
+					<Behaviors Behaviors={Behaviors}/>
+					{/* <ProfileList profiles={profiles}/> */}
+				</Grid>
+
+				<Grid item sm={4} xs={12}>
+					<p>Business...</p>
+				</Grid>
+			</Grid>
+
+
 		</main>
 	)
 };
