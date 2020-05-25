c35ee792ea8191584294cf414e3a5853e7f5c221
opeyemi-jonah <gavrieljonah@gmail.com>
2020-05-25 10:04:08 -0600

    safe commit

--- app/src/pages/business/Business.js
+++ app/src/pages/business/Business.js
@@ -17,10 +17,19 @@ export const Business = ({business,behaviors, votes}) => {
 
 
 			<>
-
+			{/* <img src = "https://s3-media3.fl.yelpcdn.com/bphoto/yRsxU5SyPhsSxMa9d8SZjw/o.jpg" alt=""/> */}
+		
+			
 			<Card className="my-5 border border-dark alternate-bg ml-5 ">
-			{/* <Card.Media image={business.businessAvatar} /> */}
+			<Card.Media
+			component = "img"
+			height="150"
+			width="150"
+			 image={business.businessAvatar} 
+				 title="business photo"
+			 />
 				<Card.Title>{business.businessName}</Card.Title>
+
 				<a href={business.businessUrl}>Yelp Link for business</a>
 				<Card.Body className="row my-3 px-5">
 				<div className="col-4 pt-5" style={{width:25}}>
