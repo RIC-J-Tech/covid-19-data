767826dc6ad9a6eacdc1fd2a7c475e94a7128d04
opeyemi-jonah <gavrieljonah@gmail.com>
2020-05-25 18:37:25 -0600

    fixed conflict

--- app/src/pages/home/Home.js
+++ app/src/pages/home/Home.js
@@ -1,37 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import {useSelector, useDispatch} from "react-redux";
-<<<<<<< HEAD
-=======
-
->>>>>>> test design for profile page looking pretty good
-// import {ProfileList} from "../profile/ProfileList";
-// // import {getAllProfiles, getProfileByEmail} from "../../shared/actions/get-profile";
-// import Grid from '@material-ui/core/Grid';
-// import axios from 'axios';
-// import Profile from '../profile/Profile'
-<<<<<<< HEAD
-=======
-
->>>>>>> test design for profile page looking pretty good
 import {getBusinessesByBusinessName, getTopBusinesses} from "../../shared/actions/get-businesses";
-// import {BusinessList} from "../business/BusinessList";
 import {getAllBehaviors} from "../../shared/actions/get-behaviors";
 import {TestBusiness} from "../business/TestBusiness";
 import {getAllVotes} from "../../shared/actions/get-votes";
-// import profileReducer from '../../shared/reducers/profileReducer';
-// import {Button, Form, FormControl} from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 import { SearchFormContent } from '../../shared/components/SearchForm/SearchForm';
-<<<<<<< HEAD
-export const Home= () => {
-	const [searchWord, setSearchWord] = useState('');
-=======
 
 export const Home= () => {
 
 	const [searchWord, setSearchWord] = useState('');
 
->>>>>>> test design for profile page looking pretty good
 	// use selector to set users to users stored in state
 	const businessState = useSelector(state => state.businesses ? state.businesses : []);
 	const behaviors = useSelector(state => state.behaviors ? state.behaviors : []);
@@ -66,19 +45,13 @@ export const Home= () => {
 			</Navbar>
 			{
 				businesses.map(
-<<<<<<< HEAD
-					business => <TestBusiness key={business.businessId} business={business} searchWord={searchWord} behaviors={behaviors.filter(behavior =>
-						behavior.behaviorBusinessId === business.businessId
-					)}
-					/>)
-=======
+
 			business => <TestBusiness key={business.businessId} business={business} searchWord={searchWord} behaviors={behaviors.filter(behavior =>
 				behavior.behaviorBusinessId === business.businessId
 			)} 
 
 			/>)
 
->>>>>>> test design for profile page looking pretty good
 			}
 			{/*<BusinessList businesses={businesses}/>*/}
 		</main>
