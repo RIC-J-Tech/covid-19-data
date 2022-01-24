353431a151916f3bf5617907a9c7cb3a1da3e339
opeyemi-jonah <ojonah@cnm.edu>
2022-01-24 06:22:21 -0700

    updated composer

--- php/vendor/composer/autoload_real.php
+++ php/vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInitcac692bd16252ad9081dcd17327f7a4e
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitcac692bd16252ad9081dcd17327f7a4e', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitcac692bd16252ad9081dcd17327f7a4e', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitcac692bd16252ad9081dcd17327f7a4e::getInitializer($loader));
         } else {
